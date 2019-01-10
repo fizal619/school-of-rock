01d646263b8fa14296b7b6d508b4403765a8e672
Fizal Sarif <fizal.sarif@gmail.com>
2019-01-10 12:58:14 -0500

    add layout header and views for clearance

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/index'
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
